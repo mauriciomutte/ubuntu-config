638f13d89e3a58ac39db02f939c141f797690d37
mauriciomutte <litiobr@gmail.com>
2020-01-16 16:52:41 -0300

    Feat(setup): add Essentials downloads

--- setup.bash
+++ setup.bash
@@ -13,3 +13,52 @@ yellow_text(){
 green_text(){
   echo -e "\e[32m$@\e[39m"
 }
+
+# -------- ESSENTIALS --------
+yellow_text "-------- ESSENTIALS --------"
+
+# -> Configure GIT
+blue_text "Configuring git configs (mauriciomutte <litiobr@gmail.com)"
+git config --global user.email "litiobr@gmail.com"
+git config --global user.name "mauriciomutte"
+
+# -> Install Curl
+echo ""
+green_text "> Installing curl (using apt)"
+sudo apt install curl -y
+
+# -> Install Flatpak
+echo ""
+green_text "> Installing flatpak (using apt)"
+sudo apt install flatpak -y
+
+echo ""
+green_text "> Installing gnome-software-plugin-flatpak (using apt)"
+sudo apt install gnome-software-plugin-flatpak -y
+
+# -> Configure Flathub
+echo ""
+green_text "> Adding flathub to flatpak"
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+# -> Install ZSH
+echo ""
+green_text "> Installing ZSH (using snap)"
+sudo apt install zsh
+zsh --version
+chsh -s $(which zsh) # Make default
+
+# -> Install Oh My Zsh
+echo ""
+green_text "> Installing Oh My Zsh (using curl)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# -> Configure Oh My Zsh
+echo ""
+green_text "> Configuring Oh My Zsh"
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zplugin/master/doc/install.sh)"
+
+rm --force ~/.zshrc
+mv /tmp/personal-setup/configs/.zshrc ~/
