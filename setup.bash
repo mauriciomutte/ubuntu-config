c2a88e134e298c1c08d2302db8e431ce92d518f9
mauriciomutte <litiobr@gmail.com>
2020-01-16 16:54:29 -0300

    Feat(setup): add theme setup

--- setup.bash
+++ setup.bash
@@ -174,3 +174,8 @@ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
 sudo apt update && sudo apt install yarn
+
+
+# -------- THEME SETUP --------
+yellow_text "-------- THEME SETUP --------"
+bash /tmp/personal-setup/theme.bash
